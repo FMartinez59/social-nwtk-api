4897fe4f5d6d1bc8f0f162b17e301d77fa18789d
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-07-04 15:51:13 -0400

    fixxed user model now working on thought model

--- models/User.js
+++ models/User.js
@@ -1,35 +1,49 @@
-const mongoose = require("mongoose");
+const {Schema, model} = require("mongoose");
 
 var validateEmail = function (email) {
   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return re.test(email);
 };
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true, trim: true },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    validate: [validateEmail, "Please fill a valid email address"],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
     ],
   },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thought",
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
     },
-  ],
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-});
+    id: false,
+  }
+);
 userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
