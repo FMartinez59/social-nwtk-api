c4488697dfef6d381aa660198a87c5a31ff01be6
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-07-09 21:29:16 -0400

    finsihed and routes work

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,27 @@
 const router = require("express").Router();
 const {
-    getAllThought
-} = require('../../controllers/thoughtController')
+  getAllThought,
+  createThought,
+  getThoughtById,
+  deleteThought,
+  updateThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+router.route("/").get(getAllThought).post(createThought);
+
+// /api/thoughts/:id
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
